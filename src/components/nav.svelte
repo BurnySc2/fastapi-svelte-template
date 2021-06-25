3c03c41044668f0e8c7ac879a32baf61dcff2b82
BurnySc2 <gamingburny@gmail.com>
2021-06-26 00:51:10 +0200

    Remove svelte routing

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,13 +1,8 @@
 <script>
-  // @ts-ignore
-import { Router, Link, Route } from "svelte-routing";
 </script>
 
-<Router>
-    <nav class="flex justify-center w-full">
-        <a class="mx-4 text-lg" href="/">Home</a>
-        <Link class="mx-4 text-lg" to="about">About</Link>
-        <Route class="mx-4 text-lg" path="todo">Todo</Route>
-        <a class="mx-4 text-lg" href="todo">Todo</a>
-    </nav>
-</Router>
+<nav class="flex justify-center w-full">
+    <a class="mx-4 text-lg" href="/">Home</a>
+    <a class="mx-4 text-lg" href="about">About</a>
+    <a class="mx-4 text-lg" href="todo">Todo</a>
+</nav>
