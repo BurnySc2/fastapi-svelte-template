fb8424a741abfa108e9f0fe5d9501724fedac7a8
BurnySc2 <gamingburny@gmail.com>
2021-06-26 00:52:07 +0200

    Fix Home-page

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <nav class="flex justify-center w-full">
-    <a class="mx-4 text-lg" href="/">Home</a>
+    <a class="mx-4 text-lg" href="./">Home</a>
     <a class="mx-4 text-lg" href="about">About</a>
     <a class="mx-4 text-lg" href="todo">Todo</a>
 </nav>
