d5278940a1d17e915759106b9d82d18887483046
BurnySc2 <gamingburny@gmail.com>
2021-06-26 00:39:03 +0200

    Add svelte routing

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -1,5 +1,12 @@
-<nav class="flex justify-center w-full">
-    <a class="mx-4 text-lg" href="/">Home</a>
-    <a class="mx-4 text-lg" href="/about">About</a>
-    <a class="mx-4 text-lg" href="/todo">Todo</a>
-</nav>
+<script>
+  // @ts-ignore
+import { Router, Link, Route } from "svelte-routing";
+</script>
+
+<Router>
+    <nav class="flex justify-center w-full">
+        <a class="mx-4 text-lg" href="/">Home</a>
+        <Link class="mx-4 text-lg" to="about">About</Link>
+        <a class="mx-4 text-lg" href="todo">Todo</a>
+    </nav>
+</Router>
