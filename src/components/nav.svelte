e67a9d3291fd9a2f4579cd9abe076e7aad31e085
BurnySc2 <gamingburny@gmail.com>
2021-06-26 00:41:47 +0200

    Add Route

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -8,5 +8,6 @@ import { Router, Link, Route } from "svelte-routing";
         <a class="mx-4 text-lg" href="/">Home</a>
         <Link class="mx-4 text-lg" to="about">About</Link>
         <Route class="mx-4 text-lg" path="todo">Todo</Route>
+        <a class="mx-4 text-lg" href="todo">Todo</a>
     </nav>
 </Router>
