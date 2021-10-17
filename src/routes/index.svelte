0acee2dc156355355fc9828948de25c71be011a2
BurnySc2 <gamingburny@gmail.com>
2021-10-17 23:25:21 +0200

    Set path to base

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,24 +4,28 @@
     import About from '../pages/About.svelte'
     import NormalChat from '../pages/NormalChat.svelte'
     import { onMount } from 'svelte'
+    // import { dev } from '$app/env';
+    import { base } from '$app/paths';
+
 
     let url = ''
     let hash = ''
+    // let PATH = dev ? '' : '/fastapi-svelte-template'
 
     onMount(() => {
         hash = location.hash
         url = hash.slice(1)
-        console.log(window);
-        console.log(location);
+        if (url === "") {
+            setUrl(url)
+        }
     })
 
     const setUrl = (newUrl: string) => {
         if (url !== newUrl) {
             url = newUrl
             hash = `#${newUrl}`
-            // TODO refactor project url to env variable?
-            // window.history.replaceState({}, '','/#/about');
-            window.history.pushState({}, '', `/fastapi-svelte-template/${hash}`)
+            // window.history.replaceState({}, '',`${PATH}/${hash}`)
+            window.history.pushState({}, '', `${base}/${hash}`)
         }
     }
 </script>
